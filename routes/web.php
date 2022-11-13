93d565385008d74d4019288e373ee6ea70f459ed
Martin Dimitrov <dimitrov.system32@gmail.com>
2022-11-13 12:44:01 +0200

    Add AdminLTE template

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/manage/dashboard', 'App\Http\Controllers\Manage\DashboardController@index');
