7551ece0d0a5d1aa38467ddd0db805a8f185430c
Martin Dimitrov <dimitrov.system32@gmail.com>
2022-11-13 11:55:18 +0200

    Add AdminLTE template

---
+++ tests/Http/Controllers/Manage/DashboardControllerTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Http\Controllers\Manage;
+
+use App\Http\Controllers\Manage\DashboardController;
+use PHPUnit\Framework\TestCase;
+
+class DashboardControllerTest extends TestCase
+{
+
+}
